0a38cc9a3ab866c0b3d2f5fb9e00b3ad7391a199
Angel-AG <miniprobecat@hotmail.com>
2019-08-20 01:06:11 -0500

    Created DataFrame and categorized by type (Tweet, Reply or RT); Also started visualization using a countplot

--- TwitterViz.py
+++ TwitterViz.py
@@ -3,10 +3,10 @@
 #%%
 from IPython import get_ipython
 
-
 #%%
 import config
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 import seaborn as sns
@@ -16,15 +16,55 @@ from twython import Twython
 twitter = Twython(config.API_KEY, config.API_SECRET_KEY, oauth_version=2)
 ACCESS_TOKEN = twitter.obtain_access_token()
 
-
 #%%
 # Prepare to use Access Token
 twitter = Twython(config.API_KEY, access_token=ACCESS_TOKEN)
 
+#%%
+# Get users Tweets, Rts, Replies and Favs from timeline
+Tweets = twitter.get_user_timeline(screen_name=config.USER, count=200)
+Favs = twitter.get_favorites(screen_name=config.USER)
+
+# for fav in Favs:
+#     print(f"User: {fav['quoted_status']['user']['screen_name']} -> {fav['text']}")
 
 #%%
-# Perform a search by user to test Twython
-Tweets = twitter.get_user_timeline(screen_name=config.USER, exclude_replies=True, include_rts=False, count=200)
+# Select the columns we want to use in our DataFrame
+cols = ["screen_name", "created_at", "text", "in_reply_to_status_id_str", "type"]
 
+#%%
+# Find the type of each Tweet object and add another key called "type"
 for tweet in Tweets:
-    print(f"User: {tweet['user']['screen_name']} -> {tweet['text']}")
+    tweet["created_at"] = pd.to_datetime(tweet["created_at"], infer_datetime_format=True).normalize().date()
+    tweet["screen_name"] = tweet["user"]["screen_name"]
+    if (tweet["in_reply_to_status_id_str"] != None):
+        tweet["type"] = "Reply"
+    elif "retweeted_status" in tweet:
+        tweet["type"] = "RT"
+    else:
+        tweet["type"] = "Tweet"
+
+#%%
+# Create our DataFrame using the ids as the index and our selected columns
+TweetsPD = pd.DataFrame(data=Tweets, index=[tweets["id"] for tweets in Tweets], columns=cols)
+
+#%%
+# Separate each type
+Replies = TweetsPD[TweetsPD["type"] == "Reply"]
+OGTweets = TweetsPD[TweetsPD["type"] == "Tweet"]
+ReTweets = TweetsPD[TweetsPD["type"] == "RT"]
+
+#%%
+plt.figure(figsize=(16,16))
+plt.title("Replies frequency")
+sns.countplot(y="created_at", data=Replies)
+
+#%%
+plt.figure(figsize=(16,16))
+plt.title("Tweets frequency")
+sns.countplot(y="created_at", data=OGTweets)
+
+#%%
+plt.figure(figsize=(16,16))
+plt.title("ReTweets frequency")
+sns.countplot(y="created_at", data=ReTweets)
